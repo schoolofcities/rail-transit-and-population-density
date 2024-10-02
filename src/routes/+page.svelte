2f05b684d0d7109a95e83e1f523762c451abed8f
an-k45 <an_k27@protonmail.com>
2024-10-02 14:59:27 -0400

    Add selectors

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,32 +1,67 @@
 <script>
     import '../assets/styles.css'; 
     import TopSofC from "../lib/TopSofC.svelte";
+
+    let curCityOne = "Toronto";
+    let curCityTwo = "Chicago";
+
+    let curMetric = "Overall density";
+
+    const cities = [
+        "Chicago",
+        "Delhi",
+        "Tokyo",
+        "Toronto",
+    ];
+
+    const metrics = [
+        "Overall density",
+        "Modified density",
+        "Transit density"
+    ];
+
+    function handleCityOneChange(event) {
+        const selectedValue = event.target.value;
+
+    }
 </script>
 
 <svelte:head>
-	<meta
-		name="viewport"
-		content="width=device-width, initial-scale=1, minimum-scale=1"
-	/>
+    <meta
+        name="viewport"
+        content="width=device-width, initial-scale=1, minimum-scale=1"
+    />
 </svelte:head>
 
 <TopSofC />
 
 <main>
-	<div class="title">
-		<h1>Comparing Transit-Oriented Density</h1>
-		<p>
-			Aniket Kali, Jeff Allen
-		</p>
-		<p>
-			Published TBD
-		</p>
-	</div>
+    <div class="title">
+        <h1>Comparing Transit-Oriented Density</h1>
+        <p>
+            Aniket Kali, Jeff Allen
+        </p>
+        <p>
+            Published TBD
+        </p>
+    </div>
 
     <div class="text">
         <p>Info/background on project...</p>
         <h3>Compare Cities</h3>
-        ... comparison ...
+        <div>
+            <select bind:value={curCityOne}>
+                {#each cities as value}
+                    <option {value}>{value}</option>
+                {/each}
+            </select>
+
+            <select bind:value={curCityTwo}>
+                {#each cities as value}
+                    <option {value}>{value}</option>
+                {/each}
+            </select>
+        </div>
         <p>Info on method...</p>
         <p>Commentary...</p>
     </div>
@@ -34,7 +69,13 @@
     <div class="text">
         <h3>Rank Cities</h3>
         <p>Info/background...</p>
-        ... ranking ...
+        <div>
+            <select bind:value={curMetric}>
+                {#each metrics as value}
+                    <option {value}>{value}</option>
+                {/each}
+            </select>
+        </div>
     </div>
 
     <div class="text">
