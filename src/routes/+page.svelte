cf74c1af093563091176394375b7b208024e9058
an-k45 <an_k27@protonmail.com>
2024-10-02 10:28:42 -0400

    Add basic website template

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,18 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+    import '../assets/styles.css'; 
+    import TopSofC from "../lib/TopSofC.svelte";
+</script>
+
+<svelte:head>
+	<meta
+		name="viewport"
+		content="width=device-width, initial-scale=1, minimum-scale=1"
+	/>
+</svelte:head>
+
+<TopSofC />
+
+<main>
+    <h1>Welcome to SvelteKit</h1>
+    <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+</main>
