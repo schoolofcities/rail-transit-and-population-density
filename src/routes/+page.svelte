54575355467c94d38b9b878d49803fa1cba4f4e8
an-k45 <an_k27@protonmail.com>
2024-10-02 16:54:14 -0400

    Improve selector styling

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -49,19 +49,26 @@
     <div class="text">
         <p>Info/background on project...</p>
         <h3>Compare Cities</h3>
-        <div>
+        <div class="city-compare">
             <select bind:value={curCityOne}>
                 {#each cities as value}
                     <option {value}>{value}</option>
                 {/each}
             </select>
 
+            {curCityOne}
+        </div>
+
+        <div class="city-compare">
             <select bind:value={curCityTwo}>
                 {#each cities as value}
                     <option {value}>{value}</option>
                 {/each}
             </select>
+
+            {curCityTwo}
         </div>
+
         <p>Info on method...</p>
         <p>Commentary...</p>
     </div>
@@ -69,13 +76,12 @@
     <div class="text">
         <h3>Rank Cities</h3>
         <p>Info/background...</p>
-        <div>
-            <select bind:value={curMetric}>
-                {#each metrics as value}
-                    <option {value}>{value}</option>
-                {/each}
-            </select>
-        </div>
+
+        <select bind:value={curMetric}>
+            {#each metrics as value}
+                <option {value}>{value}</option>
+            {/each}
+        </select>
     </div>
 
     <div class="text">
@@ -84,3 +90,14 @@
         <p>Link to code/data...</p>
     </div>
 </main>
+
+<style>
+    .city-compare {
+        width: 49.5%;
+        display: inline-block;
+    }
+
+    select {
+        width: 100%;
+    }
+</style>
