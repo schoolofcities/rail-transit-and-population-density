9a01670dd72ca2dd232b2d8729451c12fcd413b5
an-k45 <an_k27@protonmail.com>
2024-12-04 14:32:35 -0500

    Edit main writing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -93,25 +93,17 @@
 	<div class="text">
 		
 		<p>
-			Well planned public transit routes and stations are sited to serve and connect the most amount of people as possible. Yet there's a lot of variation within and between cities in terms of where routes and stations are located relative to where people live.
+			Public transit is how cities connect people to employment, education, and allow easy participation in urban life. Ideally, this is done efficiently and sustainably, with transit routes and stations connecting the most amount of people possible. In reality, there's a lot of variation within and between cities in how effectively this is done.
 		</p>
 		<p>
-			To look at this, we've created maps of major rail transit lines and stations overlaid onto population density for {cities.length} of the most populated urban regions around the globe. Click the dropdowns to view how well transit systems serve their populations in different cities. Each map is in exactly the same scale (100km in diameter) to facilitate comparative analysis.
+			To look at this, we've created maps of major rail transit lines and stations overlaid onto population density for {cities.length} of the most populated urban regions around the globe. Click the dropdowns to view how well transit systems serve their populations in different cities. Each map is in exactly the same scale (100km in diameter) to make them easy to compare.
 		</p>
 		<p>
-			Using these maps, we've also computed a number of different metrics examining characteristics of transit oriented development, and ranked how well cities perform relative to each other. Generally, the greater the density and proportion of the population that lives near higher-order transit, the better. Rapid transit and regional rail sustainably connect people to employment, education, and participate in wide array of other activities.
+			Using these maps, we've also computed a number of different metrics examining characteristics of transit oriented development, and ranked how well cities perform relative to each other. Generally, the greater the density and proportion of the population that lives near major rail transit, the better. 
 		</p>
 		<p>
-			The data we mapped and analyzed are not perfect representations of reality, which is especially difficult when doing analysis that combines sources across many jurisdictions. So please treat this as useful for comparative analyses and looking at general trends, but not 100% definitive. At the bottom of this page we detail our data sources, methodology, assumptions, limitations, and potential next steps.
+			Our data is not a perfect representation of reality, which is hard to achieve when doing analysis that combines sources across many jurisdictions. Treat what you see below as useful for comparison and seeing general trends, but not the definite truth. At the bottom of this page we detail our data sources, methodology, assumptions, limitations, and potential next steps.
 		</p>
-
-
-		<!-- <p> -->
-			<!-- Each map below displays a population density heatmap overlayed with major transit routes.  -->
-			<!-- We show density in 1 sqkm tiles, covering a 50 km radius from city centers. Transit lines and stations are shown in purple, and only include regional rail, subways, and LRT. -->
-		<!-- </p> -->
-
-		<!-- <h3>Compare Cities</h3> -->
 
 		<br>
 
@@ -178,7 +170,7 @@
 		<h3>Data sources</h3>
 
 		<p>
-			Cities included are from a dataset by <a href="https://www.naturalearthdata.com/downloads/10m-cultural-vectors/10m-populated-places/" target="_blank">Natural Earth</a>, which includes point coordinates for the centre of each city. For each city, we then defined the urban region shown on the maps as a circle with a 50km radius from this centre point.  We chose to use a standard circle size for all regions to account for idiosyncrasies in how different parts of the world define metro areas. 50km is approximately the outer range that someone would commute to/from a city centre along a major rail corridor.
+			Cities included are from a dataset by <a href="https://www.naturalearthdata.com/downloads/10m-cultural-vectors/10m-populated-places/" target="_blank">Natural Earth</a>, which includes point coordinates for the centre of each city. For each city, we then defined the urban region shown on the maps as a circle with a 50km radius from this centre point. We chose to use a standard circle size for all regions to account for idiosyncrasies in how different parts of the world define metro areas. 50km is approximately the outer range that someone would commute to/from a city centre along a major rail corridor.
 		</p>
 
 		<p>
@@ -186,7 +178,7 @@
 		</p>
 
 		<p>
-			We sourced the population density data from <a href="https://github.com/lulingliu/GlobPOP" target="_blank">GlobPOP</a>. Urban population density metrics are computed after removing areas where population density is less than 400km², to account for how some regions have more or less agricultural land and habitable geography (e.g. mountains, water, etc.).
+			We sourced the population density data from <a href="https://github.com/lulingliu/GlobPOP" target="_blank">GlobPOP</a>. Urban population density metrics are computed after removing areas where population density is less than 400km², following [Statistics Canada](https://www12.statcan.gc.ca/census-recensement/2021/ref/dict/az/definition-eng.cfm?ID=geo049a) to account for how some regions vary in agricultural land and habitable geography (e.g. mountains, water, etc.).
 		</p>
 
 		<p>
@@ -197,12 +189,12 @@
 			There are two main limitations with this transit data: 1) it only includes rail transit, not Bus Rapid Transit (BRT), which in many cities provides comparable service to rail. 2) does not account for frequency (i.e. headway) of routes. While many transit agencies share their routes and schedules in GTFS format, which includes information about frequency and often technology (bus, rail, etc.), we found that the availability of GTFS at a global scale was not available, particularly outside of Europe and North America. 
 		</p>
 
-		<!-- <p>
-			Now of course, where people live is just one side of the ....mention would be great to have destination data.
-		</p> -->
+		<p>
+			Now of course, where people live is just one side of the problem; the goal of transit is ultimately to take people where they want to go. We leave it to a future project to use destination data to understand how well cities achieve this. 
+		</p>
 
 		<p>
-			All code and data is available on our <a href="https://github.com/schoolofcities/world-city-transit-density/tree/main" target="_blank">GitHub repository</a>. 
+			All code and relevant data is available on our <a href="https://github.com/schoolofcities/world-city-transit-density/tree/main" target="_blank">GitHub repository</a>. 
 		</p>
 
 		<br>
